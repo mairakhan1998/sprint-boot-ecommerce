f06f0e1548a232ec2438c4253475427aa848a974
mairakhan1998 <munshat19@gmail.com>
2024-05-31 15:55:34 +0530

    findByCategoryId method decl. added

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -2,13 +2,17 @@ package com.luv2code.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 		System.out.println("completed");
+		//System.out.println("Spring Core Version:- " + SpringVersion.getVersion());
 	}
 
 }
--- src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -1,11 +1,17 @@
 package com.luv2code.ecommerce.dao;
 
+import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
 }
